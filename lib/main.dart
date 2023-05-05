e2c68947cba34942d29834aee5575b9e8bc439a9
Mohamed Yasser <mohamedhcjdivdjvy@gmail.com>
2023-05-05 03:45:17 +0300

    WebSocket

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,12 +8,60 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
+  TextEditingController controller = TextEditingController();
+  final chanel =
+      WebSocketChannel.connect(Uri.parse("wss://echo.websocket.events"));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("WebSocket"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Form(
+              child: TextFormField(
+                controller: controller,
+                decoration: const InputDecoration(labelText: 'Send a message'),
+              ),
+            ),
+            const SizedBox(height: 24),
+            StreamBuilder(
+              stream: chanel.stream,
+              builder: (context, snapshot) {
+                return Text(snapshot.hasData ? '${snapshot.data}' : '');
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _sendMessage,
+        tooltip: 'Send message',
+        child: const Icon(Icons.send),
+      ),
+    );
+  }
+
+  void _sendMessage() {
+    if (controller.text.isNotEmpty) {
+      chanel.sink.add(controller.text);
+    }
   }
 }
